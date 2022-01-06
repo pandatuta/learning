823abc165e4c3a3df1b4066207df78c1ea6e127b
un313 <example@un313.com>
2022-01-06 21:11:24 +0300

    First version of personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-const answer = +prompt("Age?", "18");
-console.log(answer + 1);
+const numberOfFilms = +prompt('Number of films', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Last film?', ''),
+      b = prompt('Like it?', ''),
+      c = prompt('Last film?', ''),
+      d = prompt('Like it?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
