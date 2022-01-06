61ea1d8f75e6f4cb86fd6ab3f653e8b4e884401d
un313 <example@un313.com>
2022-01-06 23:07:21 +0300

    Second version of personalMovieDB

--- js/script.js
+++ js/script.js
@@ -8,12 +8,59 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Last film?', ''),
-      b = prompt('Like it?', ''),
-      c = prompt('Last film?', ''),
-      d = prompt('Like it?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Last film?', ''),
+          b = prompt('Like it?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done!');
+    } else {
+        console.log('Error!');
+        i--;
+    }
+    
+}
+
+/* let i = 0;
+while (i < 2) {
+    const a = prompt('Last film?', ''),
+          b = prompt('Like it?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done!');
+        i++;
+    } else {
+        console.log('Error!');
+        i--;
+    }
+} */
+
+/* let i = 0;
+do {
+    const a = prompt('Last film?', ''),
+          b = prompt('Like it?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done!');
+        i++;
+    } else {
+        console.log('Error!');
+        i--;
+    }
+}
+while (i < 2); */
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
