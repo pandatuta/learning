a67dd801c333ea5b43800dfbcc379750d2716fee
un313 <example@un313.com>
2022-01-19 16:44:18 +0300

    Третья практика. Перевел всё в функции и создал две новые функции: writeYourGenres и showMyDB

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-const numberOfFilms = +prompt('Number of films', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Number of films', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Number of films', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,59 +18,59 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Last film?', ''),
-          b = prompt('Like it?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('Done!');
-    } else {
-        console.log('Error!');
-        i--;
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
     }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        const a = prompt(`You are favorite genre? id is ${i}`);
+
+        if (a != null && a != '') {
+            personalMovieDB.genres[i - 1] = a;
+        } else {
+            console.log('Error!');
+            i--;
+        }
+    }
+}
+
+writeYourGenres();
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Last film?', '');
+        const b = prompt('Like it?', '');
     
-}
-
-/* let i = 0;
-while (i < 2) {
-    const a = prompt('Last film?', ''),
-          b = prompt('Like it?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('Done!');
-        i++;
-    } else {
-        console.log('Error!');
-        i--;
-    }
-} */
-
-/* let i = 0;
-do {
-    const a = prompt('Last film?', ''),
-          b = prompt('Like it?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('Done!');
-        i++;
-    } else {
-        console.log('Error!');
-        i--;
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Done!');
+        } else {
+            console.log('Error!');
+            i--;
+        }
+        
     }
 }
-while (i < 2); */
 
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
 }
 
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+// console.log(personalMovieDB);
